7dc54f387954f9c1bae41237bd0f38b7c8a05d97
Sugeng Warsito <sugengwarsito131001@gmail.com>
2023-03-11 09:30:33 +0700

    Create Node Set PWM

---
+++ scripts/05_NodeLateral.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32
+
+def callback(data):
+    rospy.loginfo(data)
+
+    rospy.loginfo('PWM Lateral')
+    
+def main():
+    rospy.init_node('node_lateral', anonymous=True)
+
+    rospy.Subscriber("pwm_lateral", Float32, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
