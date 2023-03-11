7120cace7d673c344de82b17ec2d90cdd9d72c42
Sugeng Warsito <sugengwarsito131001@gmail.com>
2023-03-11 09:19:37 +0700

    Create Node Navigation

---
+++ scripts/01_NodeNavigation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Bool, String
+
+def callback(data):
+    rospy.loginfo(data)
+
+    pub = rospy.Publisher('position', String, queue_size=10)
+    
+    rospy.loginfo('Node Navigation')
+    
+    pub.publish('Test')
+    
+def main():
+    rospy.init_node('node_navigation', anonymous=True)
+    rospy.Subscriber("is_armed", Bool, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
