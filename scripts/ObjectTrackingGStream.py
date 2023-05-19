7b01db93f0ecd5647539a886b4bddab591f1086d
SugengW13 <sugengwarsito131001@gmail.com>
2023-05-19 20:00:15 +0700

    Create Object Detection with G Streamer

--- scripts/ObjectTrackingGStream.py
+++ scripts/ObjectTrackingGStream.py
@@ -2,19 +2,19 @@ import cv2
 import numpy as np
 from pymavlink import mavutil
 
-from ObjectDetection import HSV
 from PyMavlink import ROV
-
-cap = cv2.VideoCapture(0)
+from GStreamer import Video
+from ObjectDetection import HSV
 
 lower_hsv = np.array([0, 75, 85])
 upper_hsv = np.array([35, 255, 255])
 
-master = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
+master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 
 master.wait_heartbeat()
 
 rov = ROV(master)
+
 
 def set_rc_forward(pwm):
     rov.setRcValue(5, 1500 + pwm)
@@ -90,54 +90,59 @@ def guidance(x, y):
             pwm_lateral = calculate_pwm_lateral(distance_x)
         
         set_rc_lateral(pwm_lateral)
-    
+
+def object_detection():
+    video = Video()
+
+    while not video.frame_available():
+        print('Frame Not Available')
+
+    while True:
+        if video.frame_available():
+            frame = video.frame()
+
+            bounding_box = HSV(frame, lower_hsv, upper_hsv).get_bounding_box()
+
+            if bounding_box is not None:
+                print('Track Object')
+
+                rov.setRcValue(5, 1500)
+                
+                x, y, w, h = bounding_box
+
+                center_x = int(x + w/2)
+                center_y = int(y + h/2)
+
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.circle(frame, (center_x, center_y), 1, (0, 255, 0), 2)
+
+                guidance(center_x, center_y)
+        
+            else:
+                print('Search Object')
+
+                rov.setRcValue(5, 1550)
+
+            cv2.line(frame, (350, 0), (350, 600), (255, 0, 0), 1)
+
+            cv2.line(frame, (350, 300), (450, 300), (0, 0, 255), 1)
+            cv2.line(frame, (400, 250), (400, 350), (0, 0, 255), 1)
+
+            cv2.line(frame, (450, 0), (450, 600), (255, 0, 0), 1)
+
+            cv2.imshow('Frame', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
+
 def main():
     rov.arm()
 
     # Do some movement
 
     object_detection()
-
-def object_detection():
-    while True:
-        _, frame = cap.read()
-
-        bounding_box = HSV(frame, lower_hsv, upper_hsv).get_bounding_box()
-        
-        if bounding_box is not None:
-            print('Track Object')
-
-            rov.setRcValue(5, 1500)
-            
-            x, y, w, h = bounding_box
-
-            center_x = int(x + w/2)
-            center_y = int(y + h/2)
-
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.circle(frame, (center_x, center_y), 1, (0, 255, 0), 2)
-
-            guidance(center_x, center_y)
-        
-        else:
-            print('Search Object')
-
-            rov.setRcValue(5, 1550)
-
-        cv2.line(frame, (350, 0), (350, 600), (255, 0, 0), 1)
-
-        cv2.line(frame, (350, 300), (450, 300), (0, 0, 255), 1)
-        cv2.line(frame, (400, 250), (400, 350), (0, 0, 255), 1)
-
-        cv2.line(frame, (450, 0), (450, 600), (255, 0, 0), 1)
-
-        cv2.imshow('Frame', frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
