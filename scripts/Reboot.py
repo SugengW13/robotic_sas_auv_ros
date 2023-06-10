de4d83fd816ecc0b03c03f81c0f37f6c375a8fed
SugengW13 <sugengwarsito131001@gmail.com>
2023-06-10 15:21:56 +0700

    feat: add node_navigation.py

--- scripts/Reboot.py
+++ scripts/Reboot.py
@@ -1,7 +1,7 @@
 from PyMavlink import ROV
 from pymavlink import mavutil
 
-master = mavutil.mavlink_connection("/dev/ttyACM1", baud=115200)
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
 # master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 
 rov = ROV(master)
