c63cbe8490d1f6214e24fee9deccab87ca941eba
SugengW13 <sugengwarsito131001@gmail.com>
2023-06-13 19:56:14 +0700

    feat: create node_contol_forward

--- scripts/node_control_forward.py
+++ scripts/node_control_forward.py
@@ -11,47 +11,45 @@ class Subscriber(object):
 
         # guidance
         self.search_object = True
-        self.pwm_lateral = 1500
+        self.pwm_forward = 1500
 
         # publisher
-        self.pub_pwm_lateral = rospy.Publisher('pwm_lateral', Int16, queue_size=10)
+        self.pub_pwm_forward = rospy.Publisher('pwm_forward', Int16, queue_size=10)
 
         # subscriber
         rospy.Subscriber('search_object', Bool, self.callback_search_object)
-        rospy.Subscriber('distance_from_center', Int16, self.callback_distance_from_center)
+        rospy.Subscriber('distance_from_bottom', Int16, self.callback_distance_from_bottom)
         rospy.Subscriber('boot_time', Float32, self.callback_boot_time)
 
     def callback_search_object(self, data):
         self.search_object = data.data
 
         if self.search_object:
-            self.pwm_lateral = 1500
+            self.pwm_forward = 1500
 
-    def callback_distance_from_center(self, data):
+    def callback_distance_from_bottom(self, data):
         distance = data.data
         
-        if distance >= 50:
-            self.pwm_lateral = int(np.interp(distance, (50, 250), (1550, 1600)))
-        elif distance <= -50:
-            self.pwm_lateral = int(np.interp(distance, (-250, -50), (1400, 1450)))
+        if distance >= 150:
+            self.pwm_forward = int(np.interp(distance, (150, 450), (1550, 1600)))
+        elif distance <= 100:
+            self.pwm_forward = int(np.interp(distance, (0, 100), (1400, 1450)))
         else:
-            self.pwm_lateral = 1500
+            self.pwm_forward = 1500
         
-        if self.pwm_lateral >= 1600:
-            self.pwm_lateral = 1600
-        elif self.pwm_lateral <= 1400:
-            self.pwm_lateral = 1400
+        if self.pwm_forward >= 1600:
+            self.pwm_forward = 1600
 
     def callback_boot_time(self, data):
         boot_time = data.data
 
-        self.pub_pwm_lateral.publish(self.pwm_lateral)
+        self.pub_pwm_forward.publish(self.pwm_forward)
 
     def spin(self):
         rospy.spin()
 
 def main():
-    rospy.init_node('node_control_lateral', anonymous=True)
+    rospy.init_node('node_control_forward', anonymous=True)
 
     subscriber = Subscriber()
 
