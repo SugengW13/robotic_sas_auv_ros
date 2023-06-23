4afa160d9072f11e2a8c268b618b28696a7880c5
SugengW13 <sugengwarsito131001@gmail.com>
2023-06-23 22:45:25 +0700

    feat: integrate arg & param

--- scripts/node_messages.py
+++ scripts/node_messages.py
@@ -6,8 +6,13 @@ from std_msgs.msg import Int16, Float32
 from pymavlink import mavutil
 from PyMavlink import ROV
 
-master = mavutil.mavlink_connection("/dev/ttyACM1", baud=115200)
-# master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+pixhawk_connection = rospy.get_param('/auv/pixhawk_connection')
+pixhawk_address = rospy.get_param('/auv/pixhawk_address')
+
+if pixhawk_connection == 'usb':
+    master = mavutil.mavlink_connection(pixhawk_address, baud=115200)
+else:
+    master = mavutil.mavlink_connection(pixhawk_address)
 
 master.wait_heartbeat()
 
