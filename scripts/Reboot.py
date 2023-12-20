4a1e78f68dfe44d61c1d385809451a148fa3a04e
SugengW13 <sugengwarsito131001@gmail.com>
2023-12-20 21:05:29 +0700

    fix: adjust ros nodes

--- scripts/Reboot.py
+++
@@ -1,9 +0,0 @@
-from PyMavlink import ROV
-from pymavlink import mavutil
-
-master = mavutil.mavlink_connection("/dev/ttyACM2", baud=115200)
-# master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
-
-rov = ROV(master)
-
-rov.reboot()
