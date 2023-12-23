f996dc9d9e390137f5b7a0c13f791aebcf9b8ce7
SugengW13 <sugengwarsito131001@gmail.com>
2023-12-23 09:17:36 +0700

    fix: adjust pub/sub states & refine .launch file

--- scripts/node_control.py
+++ scripts/node_control.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int16, Float32
+from std_msgs.msg import Bool, Int16, Float32
 
 class Subscriber():
     def __init__(self):
@@ -15,20 +15,21 @@ class Subscriber():
         self.pwm_thr_8 = 1500
 
         # Publisher
-        self.pub_pwm_thr_1 = rospy.Publisher('pwm_thr_1', Int16, queue_size=10)
-        self.pub_pwm_thr_2 = rospy.Publisher('pwm_thr_2', Int16, queue_size=10)
-        self.pub_pwm_thr_3 = rospy.Publisher('pwm_thr_3', Int16, queue_size=10)
-        self.pub_pwm_thr_4 = rospy.Publisher('pwm_thr_4', Int16, queue_size=10)
-        self.pub_pwm_thr_5 = rospy.Publisher('pwm_thr_5', Int16, queue_size=10)
-        self.pub_pwm_thr_6 = rospy.Publisher('pwm_thr_6', Int16, queue_size=10)
-        self.pub_pwm_thr_7 = rospy.Publisher('pwm_thr_7', Int16, queue_size=10)
-        self.pub_pwm_thr_8 = rospy.Publisher('pwm_thr_8', Int16, queue_size=10)
+        self.pub_pwm_thr_1 = rospy.Publisher('pwm_thr_1', Float32, queue_size=10)
+        self.pub_pwm_thr_2 = rospy.Publisher('pwm_thr_2', Float32, queue_size=10)
+        self.pub_pwm_thr_3 = rospy.Publisher('pwm_thr_3', Float32, queue_size=10)
+        self.pub_pwm_thr_4 = rospy.Publisher('pwm_thr_4', Float32, queue_size=10)
+        self.pub_pwm_thr_5 = rospy.Publisher('pwm_thr_5', Float32, queue_size=10)
+        self.pub_pwm_thr_6 = rospy.Publisher('pwm_thr_6', Float32, queue_size=10)
+        self.pub_pwm_thr_7 = rospy.Publisher('pwm_thr_7', Float32, queue_size=10)
+        self.pub_pwm_thr_8 = rospy.Publisher('pwm_thr_8', Float32, queue_size=10)
 
         # Subscriber
         rospy.Subscriber('error_roll', Int16, self.callback_error_roll)
         rospy.Subscriber('error_pitch', Int16, self.callback_error_pitch)
         rospy.Subscriber('error_yaw', Int16, self.callback_error_yaw)
         rospy.Subscriber('error_depth', Float32, self.callback_error_depth)
+        rospy.Subscriber('is_start', Bool, self.callback_is_start)
 
     def callback_error_roll(self, data):
         # Kalkulasi PID
@@ -46,15 +47,18 @@ class Subscriber():
         # Kalkulasi PID
         print('Calculate PID Depth')
 
+    def callback_is_start(self, data):
+        if data.data:
+            self.pub_pwm_thr_1.publish(self.pwm_thr_1)
+            self.pub_pwm_thr_2.publish(self.pwm_thr_2)
+            self.pub_pwm_thr_3.publish(self.pwm_thr_3)
+            self.pub_pwm_thr_4.publish(self.pwm_thr_4)
+            self.pub_pwm_thr_5.publish(self.pwm_thr_5)
+            self.pub_pwm_thr_6.publish(self.pwm_thr_6)
+            self.pub_pwm_thr_7.publish(self.pwm_thr_7)
+            self.pub_pwm_thr_8.publish(self.pwm_thr_8)
+
     def spin(self):
-        self.pub_pwm_thr_1.publish(self.pwm_thr_1)
-        self.pub_pwm_thr_2.publish(self.pwm_thr_2)
-        self.pub_pwm_thr_3.publish(self.pwm_thr_3)
-        self.pub_pwm_thr_4.publish(self.pwm_thr_4)
-        self.pub_pwm_thr_5.publish(self.pwm_thr_5)
-        self.pub_pwm_thr_6.publish(self.pwm_thr_6)
-        self.pub_pwm_thr_7.publish(self.pwm_thr_7)
-        self.pub_pwm_thr_8.publish(self.pwm_thr_8)
         rospy.spin()
 
 def main():
