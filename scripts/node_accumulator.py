101fb694f238cdeaa465c41d66f7fd1271174686
SugengW13 <sugengwarsito131001@gmail.com>
2023-12-27 23:20:12 +0700

    feat: add custom msgs

--- scripts/node_accumulator.py
+++ scripts/node_accumulator.py
@@ -1,48 +1,18 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int16, Float32
+from robotic_sas_auv_ros.msg import Sensor
 
 class Subscriber():
     def __init__(self):
         # Publisher
-        self.pub_roll = rospy.Publisher('roll', Int16, queue_size=10)
-        self.pub_pitch = rospy.Publisher('pitch', Int16, queue_size=10)
-        self.pub_yaw = rospy.Publisher('yaw', Int16, queue_size=10)
-        self.pub_pressure = rospy.Publisher('pressure', Float32, queue_size=10)
-        self.pub_temperature = rospy.Publisher('temperature', Float32, queue_size=10)
-        self.pub_depth = rospy.Publisher('depth', Float32, queue_size=10)
-        self.pub_altitude= rospy.Publisher('altitude', Float32, queue_size=10)
+        self.pub_sensor = rospy.Publisher('sensor', Sensor, queue_size=10)
 
         # Subscriber
-        rospy.Subscriber('/arduino/bno_roll', Int16, self.callback_bno_roll)
-        rospy.Subscriber('/arduino/bno_pitch', Int16, self.callback_bno_pitch)
-        rospy.Subscriber('/arduino/bno_yaw', Int16, self.callback_bno_yaw)
-        rospy.Subscriber('/arduino/ms_pressure', Float32, self.callback_ms_pressure)
-        rospy.Subscriber('/arduino/ms_temperature', Float32, self.callback_ms_temperature)
-        rospy.Subscriber('/arduino/ms_depth', Float32, self.callback_ms_depth)
-        rospy.Subscriber('/arduino/ms_altitude', Float32, self.callback_ms_altitude)
+        rospy.Subscriber('/arduino/sensor', Sensor, self.callback_sensor)
 
-    def callback_bno_roll(self, data):
-        self.pub_roll.publish(data.data)
-
-    def callback_bno_pitch(self, data):
-        self.pub_pitch.publish(data.data)
-
-    def callback_bno_yaw(self, data):
-        self.pub_yaw.publish(data.data)
-
-    def callback_ms_pressure(self, data):
-        self.pub_depth.publish(data.data)
-
-    def callback_ms_temperature(self, data):
-        self.pub_depth.publish(data.data)
-
-    def callback_ms_depth(self, data):
-        self.pub_depth.publish(data.data)
-
-    def callback_ms_altitude(self, data):
-        self.pub_depth.publish(data.data)
+    def callback_sensor(self, data):
+        self.pub_sensor.publish(data)
 
     def spin(self):
         rospy.spin()
--- scripts/node_navigation.py
+++ scripts/node_navigation.py
@@ -1,66 +1,34 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Bool, Int16, Float32
+from std_msgs.msg import Bool
+from robotic_sas_auv_ros.msg import Sensor, SetPoint, Error
 
 class Subscriber():
     def __init__(self):
-        self.roll = 0
-        self.pitch = 0
-        self.yaw = 0
-        self.depth = 0.0
-        self.error_roll = 0
-        self.error_pitch = 0
-        self.error_yaw = 0
-        self.error_depth = 0.0
+        self.error = Error()
+        self.set_point = SetPoint()
 
         # Publisher
-        self.pub_error_roll = rospy.Publisher('error_roll', Int16, queue_size=10)
-        self.pub_error_pitch = rospy.Publisher('error_pitch', Int16, queue_size=10)
-        self.pub_error_yaw = rospy.Publisher('error_yaw', Int16, queue_size=10)
-        self.pub_error_depth = rospy.Publisher('error_depth', Float32, queue_size=10)
+        self.pub_error = rospy.Publisher('error', Error, queue_size=10)
 
         # Subscriber
-        rospy.Subscriber('roll', Int16, self.callback_roll)
-        rospy.Subscriber('pitch', Int16, self.callback_pitch)
-        rospy.Subscriber('yaw', Int16, self.callback_yaw)
-        rospy.Subscriber('depth', Float32, self.callback_depth)
-        rospy.Subscriber('target_roll', Int16, self.callback_target_roll)
-        rospy.Subscriber('target_pitch', Int16, self.callback_target_pitch)
-        rospy.Subscriber('target_yaw', Int16, self.callback_target_yaw)
-        rospy.Subscriber('target_depth', Float32, self.callback_target_depth)
+        rospy.Subscriber('set_point', SetPoint, self.callback_set_point)
+        rospy.Subscriber('sensor', Sensor, self.callback_sensor)
         rospy.Subscriber('is_start', Bool, self.callback_is_start)
 
-    def callback_roll(self, data):
-        self.roll = data.data
+    def callback_set_point(self, data):
+        self.set_point = data
 
-    def callback_pitch(self, data):
-        self.pitch = data.data
-
-    def callback_yaw(self, data):
-        self.yaw = data.data
-    
-    def callback_depth(self, data):
-        self.depth = data.data
-
-    def callback_target_roll(self, data):
-        self.error_roll = data.data - self.roll
-
-    def callback_target_pitch(self, data):
-        self.error_pitch = data.data - self.pitch
-
-    def callback_target_yaw(self, data):
-        self.error_yaw = data.data - self.yaw
-
-    def callback_target_depth(self, data):
-        self.error_depth = data.data - self.depth
+    def callback_sensor(self, data):
+        self.error.roll = data.roll - self.set_point.roll
+        self.error.pitch = data.pitch - self.set_point.pitch
+        self.error.yaw = data.yaw - self.set_point.yaw
+        self.error.depth = data.depth - self.set_point.depth
 
     def callback_is_start(self, data):
         if data.data:
-            self.pub_error_roll.publish(self.error_roll)
-            self.pub_error_pitch.publish(self.error_pitch)
-            self.pub_error_yaw.publish(self.error_yaw)
-            self.pub_error_depth.publish(self.error_depth)
+            self.pub_error.publish(self.error)
 
     def spin(self):
         rospy.spin()
