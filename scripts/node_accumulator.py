29cc6e856aef3ebafd1d24d702ed4189f754a45f
SugengW13 <sugengwarsito131001@gmail.com>
2024-03-15 21:40:01 +0700

    feat: store and publish object detection data

--- scripts/node_accumulator.py
+++ scripts/node_accumulator.py
@@ -3,25 +3,28 @@
 import rospy
 from std_msgs.msg import Bool
 from sensor_msgs.msg import Imu
-from robotic_sas_auv_ros.msg import Sensor
-from robotic_sas_auv_ros.msg import ArduinoSensor
+from robotic_sas_auv_ros.msg import ArduinoSensor, Sensor, BoundingBox, ObjectDetection
 from nav_msgs.msg import Odometry
+from detection_msgs.msg import BoundingBoxes
 
 class Subscriber():
     def __init__(self):
         self.sensor = Sensor()
+        self.object_detection = ObjectDetection()
 
         param_rate = rospy.get_param('/nuc/rate')
         self.rate = rospy.Rate(param_rate)
 
         # Publisher
         self.pub_sensor = rospy.Publisher('sensor', Sensor, queue_size=10)
+        self.pub_object_detection = rospy.Publisher('object_detection', ObjectDetection, queue_size=10)
 
         # Subscriber
         rospy.Subscriber('is_start', Bool, self.callback_is_start)
         rospy.Subscriber('/arduino/sensor', ArduinoSensor, self.callback_arduino_sensor)
         rospy.Subscriber('/camera/odom/sample', Odometry, self.callback_odometry)
         rospy.Subscriber('/imu', Imu, self.callback_imu)
+        rospy.Subscriber('/yolov5/detections', BoundingBoxes, self.callback_bounding_boxes)
 
     # Collect Arduino Sensor Data
     def callback_arduino_sensor(self, data: ArduinoSensor):
@@ -38,10 +41,24 @@ class Subscriber():
         self.sensor.roll = round(data.orientation.x, 3)
         self.sensor.pitch = round(data.orientation.y, 3)
         self.sensor.yaw = round(data.orientation.z, 3)
+    
+    # Collect BoundingBoxes Data
+    def callback_bounding_boxes(self, data: BoundingBoxes):
+        for b_box in data.bounding_boxes:
+            bounding_box = BoundingBox()
+            bounding_box.class_name = b_box.Class
+            bounding_box.probability = b_box.probability
+            bounding_box.x_min = b_box.xmin
+            bounding_box.y_min = b_box.ymin
+            bounding_box.x_max = b_box.xmax
+            bounding_box.y_max = b_box.ymax
+
+            self.object_detection.bounding_boxes.append(bounding_box)
 
     def callback_is_start(self, data: Bool):
         if data.data:
             self.pub_sensor.publish(self.sensor)
+            self.pub_object_detection.publish(self.object_detection)
 
         self.rate.sleep()
 
--- scripts/node_navigation.py
+++ scripts/node_navigation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Bool, String
-from robotic_sas_auv_ros.msg import Sensor, SetPoint, IsStable, Error
+from std_msgs.msg import Bool
+from robotic_sas_auv_ros.msg import Sensor, SetPoint, IsStable, Error, Movement
 
 class Subscriber():
     def __init__(self):
@@ -16,7 +16,6 @@ class Subscriber():
         # Publisher
         self.pub_error = rospy.Publisher('error', Error, queue_size=10)
         self.pub_is_stable = rospy.Publisher('is_stable', IsStable, queue_size=10)
-        self.pub_movement = rospy.Publisher('movement', String, queue_size=10)
 
         # Subscriber
         rospy.Subscriber('sensor', Sensor, self.callback_sensor)
